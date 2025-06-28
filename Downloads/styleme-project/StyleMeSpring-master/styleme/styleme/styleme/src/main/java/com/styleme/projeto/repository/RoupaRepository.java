ccf8464d495042061ad9f7d0b75b273f168e12b0
Caio_Henrique <caioh9479@gmail.com>
2025-06-27 23:01:53 -0300

    Delete Downloads/styleme-project directory

--- Downloads/styleme-project/StyleMeSpring-master/styleme/styleme/styleme/src/main/java/com/styleme/projeto/repository/RoupaRepository.java
+++
@@ -1,8 +0,0 @@
-package com.styleme.projeto.repository;
-
-import com.styleme.projeto.entity.Roupa;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.UUID;
-
-public interface RoupaRepository extends JpaRepository<Roupa, UUID> {
-}
